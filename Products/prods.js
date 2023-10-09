274d60f09429282eec5ae27c0043e9f4f8c7d106
VnceMari <104779005+Binseaaa@users.noreply.github.com>
2023-10-09 15:18:04 +0800

    Update prods.js

--- Products/prods.js
+++ Products/prods.js
@@ -6,6 +6,18 @@ let prodId = urlParams.get('product')
 fetch('https://dummyjson.com/products/' + prodId)
 .then((response) => response.json())
 .then((data) => {
+
+    document.getElementById('image').setAttribute('src', data.thumbnail)
+
+    let imageDiv = document.getElementById('images')
+    
+    data.images.forEach((imageSrc) => {
+        let image = document.createElement('img')
+        image.className = 'img'
+        image.src = imageSrc;
+
+        imageDiv.appendChild(image)
+    })
     document.getElementById('category').innerText = data.category
     document.getElementById('title').innerText = data.title
     document.getElementById('stock').innerText = data.stock
